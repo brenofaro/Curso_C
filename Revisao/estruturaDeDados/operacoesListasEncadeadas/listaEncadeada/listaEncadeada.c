f97a8aec76d3f625928cafd2df5e5c28c6741aea
Brenno Faro <94204696+brenofaro@users.noreply.github.com>
2023-06-18 16:07:29 -0300

    Navegacao em Lista encadeada

--- Revisao/estruturaDeDados/operacoesListasEncadeadas/listaEncadeada/listaEncadeada.c
+++ Revisao/estruturaDeDados/operacoesListasEncadeadas/listaEncadeada/listaEncadeada.c
@@ -11,7 +11,7 @@ typedef struct estrutura{
 
 int main(){
     
-    // Cria o inicio da lista
+    // Cria o inicio da lista (Seria tipo um cursor)
     estrutura *ponteiroEncadeado;
 
     // Cria um novo valor
@@ -24,6 +24,25 @@ int main(){
 
     // Faz o ponteiro encadeado apontar para o primeiro valor
     ponteiroEncadeado = novoPrimeiroValor;
+
+    // Cria um novo valor
+    estrutura *novoSegundoValor = (estrutura*) malloc(sizeof(estrutura));
+    novoSegundoValor->valorNumerico = 7;
+    novoSegundoValor->proximo = NULL;
+
+    // Faz o primeiro valor apontar para o segundo valor
+    ponteiroEncadeado->proximo = novoSegundoValor;
+
+    // Imprime a lista encadeda
+    while (ponteiroEncadeado != NULL)
+    {
+        // Imprime o valor atual
+        printf("Valor: %d\n", ponteiroEncadeado->valorNumerico);
+
+        // Cursor recebe a posição de memoria do proximo valor
+        ponteiroEncadeado = ponteiroEncadeado->proximo;
+    }
+    
 
     return 0;
 }
