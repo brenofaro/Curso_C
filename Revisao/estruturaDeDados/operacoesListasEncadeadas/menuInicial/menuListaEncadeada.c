fadcbbb9e1a847baed00a4d15a2f4cb970b88956
Brenno de Faro <brenofaro97@hotmail.com>
2023-06-21 11:11:35 -0300

    Criando lista encadeada, tamanho e imprimindo

--- Revisao/estruturaDeDados/operacoesListasEncadeadas/menuInicial/menuListaEncadeada.c
+++ Revisao/estruturaDeDados/operacoesListasEncadeadas/menuInicial/menuListaEncadeada.c
@@ -4,12 +4,102 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef struct Pessoa
+{
+    char nome[50];
+    int rg;
+    struct Pessoa *proximo;
+}Pessoa;
 
 
-void imprimir_menu()
+void hr();
+int tamanhoLista(Pessoa *ponteiroEncadeado);
+void imprimir_menu(Pessoa *ponteiroEncadeado);
+void imprime_encadeada(Pessoa *ponteiroEncadeado);
+
+
+int main(){
+    int funcaoDesejada = 0;
+    char tempInput[20];
+
+    // Cria o inicio da lista encadeada
+    Pessoa *ponteiroEncadeado = NULL;
+
+    // Cria o primeiro valor
+    Pessoa *novoPrimeiroValor = (Pessoa *) malloc(sizeof(Pessoa));
+    // Atribui valores ao primeiro valor
+    novoPrimeiroValor->rg = 123456;
+    strcpy(novoPrimeiroValor->nome, "Carolina");
+    novoPrimeiroValor->proximo = NULL;
+
+    // Transforma o ponteiro do inicio da lista no novo valor
+    ponteiroEncadeado = novoPrimeiroValor;
+
+    // Cria o segundo valor
+    Pessoa *novoSegundoValor = (Pessoa *) malloc(sizeof(Pessoa));
+    // Atribui valores ao segundo valor
+    novoSegundoValor->rg = 654321;
+    strcpy(novoSegundoValor->nome, "Joao");
+    novoSegundoValor->proximo = NULL;
+
+    novoPrimeiroValor->proximo = novoSegundoValor;
+
+    while (funcaoDesejada != 9)
+    {
+        // Mostrando o menu
+        imprimir_menu(ponteiroEncadeado);
+
+        // Lendo o input do usuario
+        fgets(tempInput, 20, stdin);
+        funcaoDesejada = atoi(tempInput);
+
+        // Realizar as acoes de acordo com a escolha do usuario
+        switch (funcaoDesejada)
+        {
+        case 8:
+            printf("Imprimindo lista...\n");
+            hr();
+            imprime_encadeada(ponteiroEncadeado);
+            break;
+
+        case 9:
+            printf("Saindo do programa...\n");
+            break;
+        
+        default:
+            break;
+        }
+
+    }
+    
+    return 0;
+}
+
+void hr()
+{
+    printf("---------------------------------------------------------------\n");
+}
+
+int tamanhoLista(Pessoa *ponteiroEncadeado)
+{
+    int tamanho = 0;
+    // Cria um ponteiro auxiliar para percorrer a lista
+    Pessoa *ponteiroAuxiliar = ponteiroEncadeado;
+    // Percorre a lista enquanto o ponteiro auxiliar nao for nulo
+    while (ponteiroAuxiliar != NULL)
+    {
+        tamanho++;
+        ponteiroAuxiliar = ponteiroAuxiliar->proximo;
+    }
+    
+    return tamanho;
+}
+
+void imprimir_menu(Pessoa *ponteiroEncadeado)
 {
     printf("                   OPERACOES: \n");
     hr();
+    printf("Tamanho atual da lista: %d\n", tamanhoLista(ponteiroEncadeado));
     printf("1 - Insercao de um node no inicio da lista \n");
     printf("2 - Insercao de um node no fim da lista \n");
     printf("3 - Insercao de um node na posicao N \n");
@@ -23,34 +113,19 @@ void imprimir_menu()
     printf("Escolha um numero e pressione ENTER: \n");
 }
 
-
-
-int main(){
-    int funcaoDesejada = 0;
-    char tempInput[20];
-
-    while (funcaoDesejada != 9)
+void imprime_encadeada(Pessoa *ponteiroEncadeado){
+    // Cria um ponteiro auxiliar para percorrer a lista
+    Pessoa *p = ponteiroEncadeado;
+    int i = 0;
+    while (p != NULL)
     {
-        // Mostrando o menu
-        imprimir_menu();
-
-        // Lendo o input do usuario
-        fgets(tempInput, 20, stdin);
-        funcaoDesejada = atoi(tempInput);
-
-        // Realizar as acoes de acordo com a escolha do usuario
-        switch (funcaoDesejada)
-        {
-
-        case 9:
-            printf("Saindo do programa...\n");
-            break;
-        
-        default:
-            break;
-        }
-
+        printf("Pessoa %d:\n", i);
+        printf("Nome: %s\n", p->nome);
+        printf("RG: %d\n", p->rg);
+        hr();
+        p = p->proximo;
+        i++;
     }
     
-    return 0;
+
 }
