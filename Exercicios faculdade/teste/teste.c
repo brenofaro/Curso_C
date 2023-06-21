7f3183d4f0dd9c5ab167a27b1dff1d0e4c57f88e
Brenno de Faro <brenofaro97@hotmail.com>
2023-06-21 10:47:51 -0300

    Codigo de teste

--- Exercicios faculdade/teste/teste.c
+++ Exercicios faculdade/teste/teste.c
@@ -4,41 +4,25 @@
 #define TAM 10
 
 
-void ler_vetor(int *vetor, int tam)
-{
-    printf("Vetor:\n");
-    for (int i = 0; i < tam; i++)
-    {
-        if (i == tam - 1)
-        {
-            printf("|%d|", vetor[i]);
-        }
-        else
-        {
-            printf("|%d| ", vetor[i]);
-        }
-    }
-    printf("\n");
-}
+
 
 int main()
 {
-    char nome[30];
-    nome[0] = 'a';
-    int *p1, *p2, *p3, *p4, x=0;
-    int vetor[] = {1,2,3,4,5};
-    x=0;
-    p1 = &x;
-    p2 = ++p1;
-    p3 = p2 + 4;
-    p4 = p3 - 5;
+    char letra;
+    char *texto;
+    int num;
+    bool boleano;
+    float numReal;
+    double doubleNum;
+    int *ponteiro;
 
-    if (p1 > p2)
-    {
-        printf("P1 é maior que p2\n");
-    }else{
-        printf("p2 é maior que p1");
-    }
+    printf("Tam char: %d\n", sizeof(letra));
+    printf("Tam string: %d\n", sizeof(texto));
+    printf("Tam int: %d\n", sizeof(num));
+    printf("Tam ponteiro: %d\n", sizeof(ponteiro));
+    printf("Tam bool: %d\n", sizeof(boleano));
+    printf("Tam float: %d\n", sizeof(numReal));
+    printf("Tam double: %d\n", sizeof(doubleNum));
     
 
 
